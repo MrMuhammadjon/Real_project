191526080743b77e382aa3b9e6be15b63b402d6b
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-03-31 17:09:19 +0500

    localStorage

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,11 @@
 
 // (Savat uchun alohida kodlar)
 
-function handleClick(params) {
-    console.log(salom);
+let catalogbtn = document.getElementById('catalog-btn');
+let HideCatet = document.querySelector('.Hide-catet')
+
+catalogbtn.addEventListener('toggle', ()=> {
+    HideCatet.style.top = '100px'
+    console.log('salom');
     
-}
+})
--- src/js/main.js
+++ src/js/main.js
@@ -33,46 +33,45 @@ function renderProducts(products1, products2, products3) {
     updatedProducts1.map((products1) => {
         return (
             catalog.innerHTML += `
-            <div class="product">
-                <div class="product-head">
-                    <div class="product-img">
-                        <img src="${products1.image}" alt="">
-                    </div>
-                    <span>
-                        <i class='bx bx-heart'></i>
-                    </span>
-                    <div class="discount">
-                        <h1>${products1.discount}%</h1>
-                    </div>
-                </div>
-                <div class="product-main">
-                    <div class="product-price">
-                        <div class="With-map">
-                            <h1>${products1.price}$</h1>
-                            <p>С картой</p>
-                        </div>
-                        <div class="Regular">
-                            <h1>${products1.paycart}$</h1>
-                            <p>Без карты</p>
-                        </div>
-                    </div>
-                    <div class="product-name">
-                        <p>
-                            ${products1.name}
-                        </p>
-                    </div>
-                    <div class="product-rating">
-                        <span>
-                            ⭐[ ${products1.rating} ]
-                        </span>
-                    </div>
-                    <button class="add-product-basket__btn" id="add-product-basket">
-                        <i class='bx bx-shopping-bag'></i>
-                        В корзину
-                    </button>
-                </div>
-            </div>
-            `
+              <div class="product">
+                  <div class="product-head">
+                      <div class="product-img">
+                          <img src="${products1.image}" alt="">
+                      </div>
+                      <span class="bx-heart-1">
+                          <i class='bx bx-heart'></i>
+                      </span>
+                      <div class="discount">
+                          <h1>${products1.discount}%</h1>
+                      </div>
+                  </div>
+                  <div class="product-main">
+                      <div class="product-price">
+                          <div class="with-map">
+                              <div class="With-map-in"><h1>${products1.price}</h1><sapn>$</sapn></div>
+                              <p>С картой</p>
+                          </div>
+                          <div class="Regular">
+                              <h1>${products1.paycart}$</h1>
+                              <p>Без карты</p>
+                          </div>
+                      </div>
+                      <div class="product-name">
+                          <p>
+                              ${products1.name}
+                          </p>
+                      </div>
+                      <div class="product-rating">
+                          <span>
+                              ⭐[ ${products1.rating} ]
+                          </span>
+                      </div>
+                      <button class="add-product-basket__btn" id="add-product-basket">
+                          <i class='bx bx-shopping-bag'></i>
+                          В корзину
+                      </button>
+                  </div>
+              </div>`
         )
     })
 
@@ -90,45 +89,45 @@ function renderProducts(products1, products2, products3) {
     updatedProducts2.map((products2) => {
         return (
             NewItemCatalog.innerHTML += `
-            <div class="product">
-                <div class="product-head">
-                    <div class="product-img">
-                        <img src="${products2.image}" alt="">
-                    </div>
-                    <span>
-                        <i class='bx bx-heart'></i>
-                    </span>
-                    <div class="discount">
-                        <h1>${products2.discount}%</h1>
-                    </div>
-                </div>
-                <div class="product-main">
-                    <div class="product-price">
-                        <div class="With-map">
-                            <h1>${products2.price}$</h1>
-                            <p>С картой</p>
-                        </div>
-                        <div class="Regular">
-                            <h1>${products2.paycart}$</h1>
-                            <p>Без карты</p>
-                        </div>
-                    </div>
-                    <div class="product-name">
-                        <p>
-                            ${products2.name}
-                        </p>
-                    </div>
-                    <div class="product-rating">
-                        <span>
-                            ⭐[ ${products2.rating} ]
-                        </span>
-                    </div>
-                    <button class="add-product-basket__btn" id="add-product-basket">
-                        <i class='bx bx-shopping-bag'></i>
-                        В корзину
-                    </button>
-                </div>
-            </div>`
+              <div class="product">
+                  <div class="product-head">
+                      <div class="product-img">
+                          <img src="${products2.image}" alt="">
+                      </div>
+                      <span class="bx-heart-1">
+                          <i class='bx bx-heart'></i>
+                      </span>
+                      <div class="discount">
+                          <h1>${products2.discount}%</h1>
+                      </div>
+                  </div>
+                  <div class="product-main">
+                      <div class="product-price">
+                          <div class="with-map">
+                              <div class="With-map-in"><h1>${products2.price}</h1><sapn>$</sapn></div>
+                              <p>С картой</p>
+                          </div>
+                          <div class="Regular">
+                              <h1>${products2.paycart}$</h1>
+                              <p>Без карты</p>
+                          </div>
+                      </div>
+                      <div class="product-name">
+                          <p>
+                              ${products2.name}
+                          </p>
+                      </div>
+                      <div class="product-rating">
+                          <span>
+                              ⭐[ ${products2.rating} ]
+                          </span>
+                      </div>
+                      <button class="add-product-basket__btn" id="add-product-basket">
+                          <i class='bx bx-shopping-bag'></i>
+                          В корзину
+                      </button>
+                  </div>
+              </div>`
         )
     })
 
@@ -150,7 +149,7 @@ function renderProducts(products1, products2, products3) {
                       <div class="product-img">
                           <img src="${products3.image}" alt="">
                       </div>
-                      <span>
+                      <span class="bx-heart-1">
                           <i class='bx bx-heart'></i>
                       </span>
                       <div class="discount">
@@ -159,8 +158,8 @@ function renderProducts(products1, products2, products3) {
                   </div>
                   <div class="product-main">
                       <div class="product-price">
-                          <div class="With-map">
-                              <h1>${products3.price}$</h1>
+                          <div class="with-map">
+                              <div class="With-map-in"><h1>${products3.price}</h1><sapn>$</sapn></div>
                               <p>С картой</p>
                           </div>
                           <div class="Regular">
@@ -198,52 +197,67 @@ renderProducts(aksiiProducts, novinkiProducts, pokupaliProducts);
 let olibbolinganItem = []
 
 let getProductBtn = document.querySelectorAll(".add-product-basket__btn");
+let getProductHeart = document.querySelectorAll(".bx-heart-1")
+let basketCount = document.getElementById("basket-count")
+let heartCount = document.getElementById("sort-count")
+let Zbasket = 0
+let Zheart = 0
+
+
 
 
 getProductBtn.forEach((btn) => {
+    let chekedBasket = false
+
     btn.addEventListener('click', () => {
-        let productName = btn.parentElement.querySelector(".product-name p").innerHTML
-        let productPrice = btn.parentElement.querySelector(".With-map h1").innerHTML
-        let productImg = btn.parentElement.parentElement.querySelector(".product-img img").src
-        let productRating = btn.parentElement.querySelector(".product-rating span").innerHTML
+        let productName = btn.parentElement.querySelector(".product-name p").innerHTML;
+        let productPrice = btn.parentElement.querySelector(".With-map-in h1").innerHTML;
+        let productImg = btn.parentElement.parentElement.querySelector(".product-img img").src;
+        let productRating = btn.parentElement.querySelector(".product-rating span");
 
-        olibbolinganItem.push({
-            name: productName,
-            src: productImg,
-            price:  productPrice,
-            rating: productRating
-        })
+        console.log(productPrice);
+        Zbasket++
+        basketCount.innerText = Zbasket
+        localStorage.setItem("counterBasket", Zbasket)
+        
+        // Obyekt sifatida saqlash yaxshiroq
+        if(!chekedBasket){
+            chekedBasket = true
+            olibbolinganItem.push({
+                name: productName,
+                price: productPrice,
+                image: productImg,
+                rating: productRating
+            });
+        }else{
+            olibbolinganItem.unshift()
+        }
 
-        
-        
-    })
+        console.log(olibbolinganItem);
+    });
 })
 
-console.log(olibbolinganItem);
+getProductHeart.forEach((btn, index) => {
+    let chekedHart = false
+
+    btn.addEventListener('click', () => {
+
+        if (!chekedHart) {
+            Zheart++;
+            heartCount.innerText = Zheart;
+            chekedHart = true;
+            btn.style.backgroundColor = "#70C05B"
+        } else {
+            Zheart--
+            heartCount.innerText = Zbasket
+            chekedHart = false
+            btn.style.backgroundColor = "#8F8F8F"
+        }
+
+    })
+
+})
 
 
 
-
-// let olibbolinganItem = []; // Massivni funktsiyadan oldin e'lon qilish kerak
-
-// let getProductBtn = document.querySelectorAll(".add-product-basket__btn");
-
-// getProductBtn.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//         let productName = btn.parentElement.querySelector(".product-name p").innerHTML;
-//         let productPrice = btn.parentElement.querySelector(".with-map h1"); 
-//         let productImg = btn.parentElement.parentElement.querySelector(".product-img img").src;
-//         let productRating = btn.parentElement.querySelector(".product-rating span");
-
-//         // Obyekt sifatida saqlash yaxshiroq
-//         olibbolinganItem.push({
-//             name: productName,
-//             price: productPrice,
-//             image: productImg,
-//             rating: productRating
-//         });
-
-//         console.log(olibbolinganItem);
-//     });
-// });
 
